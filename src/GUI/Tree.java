6cae6c5ff36f8cac0f91095da365d26e8165d7c1
PhilippDE <p.dolif@gmail.com>
2017-04-06 16:56:32 +0200

    Fixed popupmenu bug

--- src/GUI/Tree.java
+++ src/GUI/Tree.java
@@ -28,17 +28,16 @@ public class Tree extends JTree {
             Node node = getNode(path);
             if(node != null) {
                 Rectangle bounds = getRowBounds(i);
-                Rectangle clipBounds = g.getClipBounds();
                 double percent = node.getUsagePercentOfParent();
                 Color c = new Color(Color.HSBtoRGB((float)(1-percent)/3, 1, 1));
                 g.setColor(node.getOwnColor());
-                int boxWidth = (int)((clipBounds.getWidth()/4) * percent);
-                g.fillRect((int)(clipBounds.getWidth()-boxWidth)-3, (int)bounds.getY()+5, boxWidth, (int)bounds.getHeight()-9);
+                int boxWidth = (int)((getWidth()/4) * percent);
+                g.fillRect((getWidth()-boxWidth)-3, (int)bounds.getY()+5, boxWidth, (int)bounds.getHeight()-9);
                 //g.setColor(Color.BLACK);
                 //g.drawLine((int)(clipBounds.getWidth()*0.75-3), (int)bounds.getY(),(int)(clipBounds.getWidth()*0.75-3),(int)(bounds.getY()+bounds.getHeight()-5));
                 g.setColor(new Color(210,210,210));
-                g.drawRect((int)((clipBounds.getWidth())*0.75)-5, (int)bounds.getY()+3,
-                        (int)((clipBounds.getWidth())*0.25)+4, (int)bounds.getHeight()-6);
+                g.drawRect((int)((getWidth())*0.75)-5, (int)bounds.getY()+3,
+                        (int)((getWidth())*0.25)+4, (int)bounds.getHeight()-6);
             }
         }
     }
